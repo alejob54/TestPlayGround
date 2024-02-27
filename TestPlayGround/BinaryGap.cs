7d3f9c7ae9c06d739a1cb742800a08700ad5f125
alejo <alejo@ALEJANDROBA0A5F>
2024-02-27 15:53:01 -0600

    Agregar archivos de proyecto.

---
+++ TestPlayGround/BinaryGap.cs
@@ -0,0 +1,55 @@
+﻿namespace TestPlayGround
+{
+    public class BinaryGap
+    {
+        public int solution(int N)
+        {
+            // Implement your solution here
+            string binary = Convert.ToString(N, 2);
+
+            //Validate if the binary contains 0 to give a faster response 
+            if (binary.Contains("0"))
+            {
+                //List to store all the gaps
+                List<int> Gaps = new List<int>();
+                
+                //Flag to handle sequences (gaps)
+                bool SequenceStarted = false;
+                int max = 0;
+                for (int i = 0; i < binary.Length; i++)
+                {
+                    //Validate if the number is 1 or 0
+                    if (Convert.ToInt16(binary[i]).Equals(49))
+                    {
+                        //Sequence already started, so we need to end the gap count.
+                        if (SequenceStarted)
+                        {
+                            if (max != 0)
+                            {
+                                Gaps.Add(max);
+                                max = 0;
+                            }
+                            continue;
+                        }
+                        else
+                        {
+                            SequenceStarted = true;
+                        }
+                    }
+                    else //Value is 0
+                    {
+                        if (SequenceStarted)
+                        {
+                            max++;
+                        }
+                    }
+                }
+                return Gaps.Any() ? Gaps.Max() : 0;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ TestPlayGround/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TestPlayGround;
+
+Console.WriteLine("Hello, World!");
+
+
+Console.WriteLine(new BinaryGap().solution(1010));
