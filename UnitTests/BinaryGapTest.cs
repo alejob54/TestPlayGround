185498536562863029bec2fc368bf87a37ccf2b7
Alejandro <alejob54@gmail.com>
2024-02-27 16:43:09 -0600

    Add more tests extracted from codility website.

--- UnitTests/BinaryGapTest.cs
+++ UnitTests/BinaryGapTest.cs
@@ -10,6 +10,13 @@ namespace UnitTests
         [InlineData(32, 0)]   // 100000
         [InlineData(9, 2)]    // 1001
         [InlineData(529, 4)]  // 1000010001
+        [InlineData(805306373, 25)]  // 110000000000000000000000000101
+        [InlineData(74901729, 4)]  // 100011101101110100011100001
+        [InlineData(66561, 9)]  // 10000010000000001
+        [InlineData(561892, 3)]  // 10001001001011100100
+        [InlineData(2147483647, 0)]
+        [InlineData(1, 0)]
+        [InlineData(5, 1)]
         public void Solution_Returns_CorrectResult(int N, int expected)
         {
             // Arrange
