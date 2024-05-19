ca5c4469c96b626ea15ffed230cc3147c7cca667
alejob54 <alejob54@gmail.com>
2024-05-18 19:19:05 -0600

    attempt 1
    attempt to solve inverse tree.

---
+++ TestPlayGround/14. BinarySearch/FindInverseTreeDepth.cs
@@ -0,0 +1,43 @@
+﻿namespace TestPlayGround;
+
+public class FindInverseTreeDepth
+{    
+    public int GetDepth(string preorderSequence)
+    {
+        int maxDepth = 0;
+        int currentDepth = 0;
+        int index = 0;
+        
+        // Helper function to traverse the tree
+        void Traverse()
+        {
+            if (index >= preorderSequence.Length)
+                return;
+            
+            if (preorderSequence[index] == 'X')
+            {
+                currentDepth++;
+                if (currentDepth > maxDepth)
+                {
+                    maxDepth = currentDepth;
+                }
+                
+                index++;
+                Traverse(); // Traverse left subtree
+                
+                index++;
+                Traverse(); // Traverse right subtree
+                
+                currentDepth--; // Go back up after both subtrees are traversed
+            }
+            else if (preorderSequence[index] == 'Y')
+            {
+                index++;
+            }
+        }
+        
+        Traverse();
+        return maxDepth;
+        
+    }
+}
---
+++ UnitTests/FindInverseTreeDepthTests.cs
@@ -0,0 +1,19 @@
+﻿using TestPlayGround;
+
+namespace UnitTests;
+
+public class FindInverseTreeDepthTests
+{
+    [Theory]
+    [InlineData("XYY", 2)]
+    [InlineData("XXYY", 3)]
+    [InlineData("XXYYXYY", 3)]
+    [InlineData("XXXYYYY", 4)]
+    [InlineData("XXXYYYXYXYXYY", 5)]
+    public void Test(string path, int depth)
+    {
+        FindInverseTreeDepth findInverseTreeDepth = new();
+        int treeDepth = findInverseTreeDepth.GetDepth(path);
+        Assert.Equal(treeDepth, depth);
+    }
+}
